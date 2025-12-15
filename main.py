99b01583f4deca61e97f5f0f495f8590ee035a5e
axolotl02 <sabrina.force@gmail.com>
2025-12-15 11:21:27 -0500

    kLAJWGSDTGALDSAB:IYDGKJLASU

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from game import Game
+
+running = True #Hello
+
+game = Game()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    game.update()
+
+pygame.quit()
+
+### TODO:
+#make mouse aim properly with rescaled camera
