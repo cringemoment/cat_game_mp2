63996497d7e13c7aab44c78e3369142f75a68050
axolotl02 <sabrina.force@gmail.com>
2025-12-19 11:42:31 -0500

    added more door classes

--- assets/tilesets/objects.tsx
+++ assets/tilesets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="128" tileheight="64" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="128" tileheight="64" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="Box">
   <image source="../objects/box.png" width="32" height="32"/>
@@ -24,5 +24,11 @@
  </tile>
  <tile id="5">
   <image source="../objects/table.png" width="64" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../objects/trapdoor_closed.png" width="64" height="32"/>
+ </tile>
+ <tile id="7" type="StartOpenTrapDoor">
+  <image source="../objects/trapdoor_open.png" width="64" height="32"/>
  </tile>
 </tileset>
--- src/physics/objects/door.py
+++ src/physics/objects/door.py
@@ -22,3 +22,69 @@ class Door(PhysicsObject, ActivatedObject):
     def on_both_leave(self):
         self.change_image("default")
         self.collision = True
+
+class ReverseDoor(PhysicsObject, ActivatedObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gravity = 0
+        self.collision = False
+        self.pushback_factor = 1
+
+        self.sprites = {
+            "closed": "objects/door_closed.png",
+            "open": "objects/door_open.png"
+        }
+
+        self.set_sprites(self.sprites)
+
+    def on_any_enter(self, player):
+        self.change_image("default")
+        self.collision = True
+
+    def on_both_leave(self):
+        self.change_image("open")
+        self.collision = False
+
+class ReverseTrapDoor(PhysicsObject, ActivatedObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gravity = 0
+        self.collision = False
+        self.pushback_factor = 1
+        self.sprites = {
+            "closed": "objects/trapdoor_closed.png",
+            "open": "objects/trapdoor_open.png"
+        }
+
+        self.set_sprites(self.sprites)
+
+    def on_any_enter(self, player):
+        self.change_image("closed")
+        self.collision = True
+
+    def on_both_leave(self):
+        self.change_image("open")
+        self.collision = False
+
+class TrapDoor(PhysicsObject, ActivatedObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gravity = 0
+        self.collision = True
+        self.pushback_factor = 1
+
+        self.sprites = {
+            "closed": "objects/trapdoor_closed.png",
+            "open": "objects/trapdoor_open.png"
+        }
+
+        self.set_sprites(self.sprites)
+
+    def on_any_enter(self, player):
+        self.change_image("open")
+        self.collision = False
+
+    def on_both_leave(self):
+        self.change_image("default")
+        self.collision = True
+
