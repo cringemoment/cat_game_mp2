e7b847b8b6df50d50bbd3ced4abefdd5e07b5a4a
Jane Cao <69696899+cringemoment@users.noreply.github.com>
2026-01-05 22:46:47 -0500

    Update fonts.py

--- src/renderer/fonts.py
+++ src/renderer/fonts.py
@@ -2,3 +2,5 @@ import pygame
 pygame.font.init()
 
 menu_font =  pygame.font.SysFont('Comic Sans MS', 30)
+title_font = pygame.font.Font('src/renderer/NewDos.ttf', 30)
+subtitle_font = pygame.font.Font('src/renderer/NewDos.ttf', 20)
