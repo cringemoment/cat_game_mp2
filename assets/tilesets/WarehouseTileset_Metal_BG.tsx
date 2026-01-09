551d402f31239bb6b4cb6bdab08351d20411fedb
axolotl02 <sabrina.force@gmail.com>
2026-01-09 16:08:34 -0500

    ...

---
+++ assets/tilesets/WarehouseTileset_Metal_BG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WarehouseTileset_Metal_BG" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="WarehouseTileset_Metal_BG.png" width="224" height="224"/>
+</tileset>
