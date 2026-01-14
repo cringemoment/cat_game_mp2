f2f7b83e92ead9db893a1c654b03359aef03b5b4
axolotl02 <sabrina.force@gmail.com>
2026-01-13 21:45:40 -0500

    platform stuff

--- src/physics/objects/platforms.py
+++ src/physics/objects/platforms.py
@@ -1,6 +1,15 @@
-'''from src.physics.physicsobject import PhysicsObject
+from src.physics.physicsobject import PhysicsObject
+from src.triggers.triggerobject import ActivatedObject, Trigger
 
 
-class HorizontalPlatform(PhysicsObject):
-    def __init__(self):
-        '''
+class HorizontalPlatform(PhysicsObject, ActivatedObject):
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gravity = 0
+        self.vely = -1
+        self.trigger_interactible = True
+    def on_any_enter(self, player):
+        self.vely = 1
+
+class VerticalPlatform(PhysicsObject, ActivatedObject):
+    pass
