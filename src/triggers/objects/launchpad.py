57c6e374665302befee1715003d2abf0350cf892
pipistrelli <pipistrelli2008@gmail.com>
2026-01-14 10:13:34 -0500

    Level 2 Background

--- src/triggers/objects/launchpad.py
+++ src/triggers/objects/launchpad.py
@@ -7,19 +7,7 @@ class LaunchPad(Sprite, Trigger):
         Sprite.__init__(self, level)
         Trigger.__init__(self, *args, **kwargs, level = level)
         self.object_interactible = True
-        self.on = False
-
-        sprites = {
-            "pressed": "objects/button_pressed.png"
-        }
-
-        self.set_sprites(sprites)
 
     def on_enter(self, player):
-        player.vely = -23
-
-    def on_both_leave(self):
-        Trigger.on_both_leave(self)
-        self.change_image("default")
-        self.level.game.sound_handler.play_sound("button")
-        self.on = False
+        player.vely = -17
+        self.level.game.sound_handler.play_sound("boing")
