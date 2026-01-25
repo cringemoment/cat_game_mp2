5cccbd39f2d8112ca335865f7bdf83d4a91c3520
axolotl02 <sabrina.force@gmail.com>
2026-01-25 14:51:46 -0500

    stuff

--- assets/objects/racoons/racoons.tsx
+++ assets/objects/racoons/racoons.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="racoons" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="racoons" tilewidth="64" tileheight="64" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../../sprites/players/racoonbow.png" width="64" height="64"/>
@@ -21,5 +21,11 @@
  </tile>
  <tile id="6">
   <image source="../../sprites/players/racoonwatch.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../../sprites/players/racoonhat - Copy.png" width="64" height="64"/>
+ </tile>
+ <tile id="8">
+  <image source="../../sprites/players/racoonpocketwatch - Copy.png" width="64" height="64"/>
  </tile>
 </tileset>
---
+++ assets/tilesets/decor objects.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="decor objects" tilewidth="64" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../objects/box.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../objects/table.png" width="64" height="32"/>
+ </tile>
+</tileset>
